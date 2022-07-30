65de1853da0fb2e7dcfbbfd867ebcf5e985a5055
SergeyT <0tx0rx@gmail.com>
2022-07-30 20:56:24 +0700

    Start

---
+++ Forms/main_form.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main_form
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main_form))
+        Me.ComboFontStyle = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ComboFontStyle
+        '
+        Me.ComboFontStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboFontStyle.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ComboFontStyle.FormattingEnabled = True
+        Me.ComboFontStyle.Location = New System.Drawing.Point(87, 7)
+        Me.ComboFontStyle.Name = "ComboFontStyle"
+        Me.ComboFontStyle.Size = New System.Drawing.Size(229, 26)
+        Me.ComboFontStyle.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(304, 401)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Шрифт"
+        '
+        'main_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(329, 461)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboFontStyle)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "main_form"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Font Viewer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboFontStyle As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Forms/main_form.vb
@@ -0,0 +1,32 @@
+﻿Public Class main_form
+
+    Public ComboFonts As New Drawing.Text.InstalledFontCollection
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim installedFonts As New System.Drawing.Text.InstalledFontCollection
+        ComboFontStyle.DataSource = installedFonts.Families
+        ComboFontStyle.DisplayMember = "Name"
+        Try
+            Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Regular)
+        Catch
+            Try
+                Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Bold)
+            Catch
+                Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Italic)
+            End Try
+        End Try
+    End Sub
+
+    Private Sub ComboFontStyle_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboFontStyle.SelectedIndexChanged
+        Try
+            Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Regular)
+        Catch
+            Try
+                Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Bold)
+            Catch
+                Label1.Font = New Font(ComboFonts.Families(ComboFontStyle.SelectedIndex), 12, FontStyle.Italic)
+            End Try
+        End Try
+    End Sub
+
+End Class
